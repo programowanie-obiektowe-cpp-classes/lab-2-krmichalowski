f33b27682255670aa6b2cecb9199d53ee5b6a368
km_laptop <km>
2025-10-22 17:39:56 +0200

    wykonanie zadania

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,63 @@
 #pragma once
 
+#include <iostream>
+
 #include "Resource.hpp"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+    Resource* m_res;
+
+    public:
+    ResourceManager() : m_res{new Resource{}}
+    {
+
+    }
+
+    ~ResourceManager()
+    {
+        delete m_res;
+    }
+
+    ResourceManager(const ResourceManager& res_menager) : m_res{new Resource{*res_menager.m_res}}
+    {
+
+    }
+
+    ResourceManager& operator=(const ResourceManager& res_menager)
+    {
+        if(this != &res_menager)
+        {
+            delete m_res;
+
+            m_res = new Resource{*res_menager.m_res};
+        }
+
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& res_menager) : m_res{res_menager.m_res}
+    {
+        res_menager.m_res = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& res_menager)
+    {
+        if(this != &res_menager)
+        {
+            delete m_res;
+
+            m_res = res_menager.m_res;
+
+            res_menager.m_res = nullptr;
+        }
+
+        return *this;
+    }
+
+    double get()
+    {
+        return m_res->get();
+    }
 };
